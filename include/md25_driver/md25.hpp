eaf25c46472111a9f9ddd4684bb9510cfe727096
Ozoid <steve@ozoid.co.uk>
2020-05-08 00:43:34 +0100

    added new function headers and registers

--- include/md25_driver/md25.hpp
+++ include/md25_driver/md25.hpp
@@ -42,7 +42,33 @@ public:
 
   bool setup();
   std::pair<long, long> get_encoders();
-private:
+    
+  void resetEncoders();
+  int getEncoder1();
+  int getEncoder2();
+  void setMotorsSpeed(byte speed);
+  void setMotor1Speed(byte speed);
+  void setMotor2Speed(byte speed);
+  void stopMotor1();
+  void stopMotor2();
+  void stopMotors();
+  long getSoftwareVersion();
+  float getBatteryVolts();
+  void changeAddress(byte newAddress);
+  byte getAccelerationRate();
+  byte getMotor1Current();
+  byte getMotor2Current();
+  byte getMotor1Speed();
+  byte getMotor2Speed();
+  byte getMode();
+  void enableSpeedRegulation();
+  void disableSpeedRegulation();
+  void enableTimeout();
+  void disableTimeout();
+  void setMode(byte mode);
+  void setAccelerationRate(byte rate);
+  
+  private:
   bool read_encoders();
   bool reset_encoders();
   void stop_motors();
@@ -57,6 +83,20 @@ private:
   long m_encoder_2_ticks = 0;
 
   const char * m_i2c_file = nullptr;
+  
+  static byte const cmdReg		= 0x10;  // command register
+  static byte const speed1Reg		= 0x00;  // speed to first motor
+  static byte const speed2Reg		= 0x01;  // speed to second motor
+  static byte const encoderOneReg	= 0x02;  // motor encoder 1 (first byte)
+  static byte const encoderTwoReg	= 0x06;  // motor encoder 2 (first byte)
+  static byte const voltReg		= 0x0A;  // battery volts
+  static byte const current1Reg	= 0x0B;  // motor 1 current
+  static byte const current2Reg	= 0x0C;  // motor 2 current
+  static byte const softwareVerReg= 0x0D;  // software version
+  static byte const accRateReg	= 0x0E;  // acceleration rate
+  static byte const modeReg		= 0x0F;  // mode of operation
+  static byte const stopSpeed		= 0x80;  // 0 velocity
+
 };
 
 #endif
